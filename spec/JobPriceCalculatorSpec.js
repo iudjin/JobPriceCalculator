e1aef36b574554e94984afb8ed6793fc67c94d9a
iudjin <eugen.gorgan@gmail.com>
2016-09-19 15:33:04 -0400

    added calculation of category markup

--- spec/JobPriceCalculatorSpec.js
+++ spec/JobPriceCalculatorSpec.js
@@ -5,7 +5,15 @@ describe("JobPriceCalculator", function() {
     calculator = new JobPriceCalculator();
   });
 
-  it("should return base price + flat markup + job markup", function() {
-    expect(calculator.getEstimatedPrice('100, 1 person')).toEqual(106.26);
+  it("should return 1591.58 for 1,299.99, 3 people, food", function() {
+    expect(calculator.getEstimatedPrice('1299.99, 3 people, food')).toEqual(1591.58);
+  });
+
+  it("should return 6199.81 for 5432.00, 1 person, drugs", function() {
+    expect(calculator.getEstimatedPrice('5432.00, 1 person, drugs')).toEqual(6199.81);
+  });
+
+  it("should return 13707.63 for 12456.95, 4 people, books", function() {
+    expect(calculator.getEstimatedPrice('12456.95, 4 people, books')).toEqual(13707.63);
   });
 });
--- src/JobPriceCalculator.js
+++ src/JobPriceCalculator.js
@@ -1,13 +1,23 @@
 function JobPriceCalculator() {
+  this.categories = {};
+  this.categories.food = 0.13;
+  this.categories.electronics = 0.02;
+  this.categories.drugs = 0.075;
 };
 
 JobPriceCalculator.prototype.getEstimatedPrice = function(input) {
   var data = input.split(',');
   var basePrice = Number(data[0].trim());
   var numberOfPeople = Number(data[1].trim().split(' ')[0]);
+  var category = data[2].trim();
 
   var flatMarkup = basePrice * 0.05;
   var jobMarkup = (basePrice + flatMarkup) * numberOfPeople * 0.012;
 
-  return Math.round((basePrice + flatMarkup + jobMarkup) * 100) / 100;
+  var categoryMarkup = 0;
+  for (var item in this.categories) {
+    if (category === item)
+      categoryMarkup = (basePrice + flatMarkup) * this.categories[item];
+  }
+  return Math.round((basePrice + flatMarkup + jobMarkup + categoryMarkup) * 100) / 100;
 };
